92ba294768f6df7d9b1cc7fc045cbadd418a8a6d
Noevil <noevil@foxmail.com>
2016-12-19 02:30:53 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-class Elastic {
-    constructor(styles) {
+import {
+    StatusBar,
+    TouchableOpacity,
+    PixelRatio
+} from 'react-native';
 
-    };
+const PR = PixelRatio.get();
+
+const ELASTIC_MAP = {
+    height: {
+        2: 1,
+        3: 1
+    },
+    fontSize: {
+        2: 1,
+        3: 1.2
+    },
+    lineHeight: {
+        2: 1,
+        3: 1.2
+    }
 };
 
-export default Elastic;
+export default {
+    create: (styles) => {
+        for (var style in styles) {
+            for (var k in styles[style]) {
+                if (ELASTIC_MAP[k] && ELASTIC_MAP[k][PR]) {
+                    styles[style][k] = styles[style][k] * ELASTIC_MAP[k][PR];
+                }
+            }
+        }
+
+        return styles;
+    }
+};
