f97745f6a4f3becdcc4a6370d5bada41a35d0421
Noevil <noevil@foxmail.com>
2016-12-19 03:05:21 +0800

    change the function name

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import {
-    StatusBar,
-    TouchableOpacity,
+    StyleSheet,
     PixelRatio
 } from 'react-native';
 
 const PR = PixelRatio.get();
-
 const ELASTIC_MAP = {
     height: {
         2: 1,
@@ -21,16 +19,23 @@ const ELASTIC_MAP = {
     }
 };
 
-export default {
-    create: (styles) => {
-        for (var style in styles) {
-            for (var k in styles[style]) {
-                if (ELASTIC_MAP[k] && ELASTIC_MAP[k][PR]) {
-                    styles[style][k] = styles[style][k] * ELASTIC_MAP[k][PR];
-                }
+let repair = (styles) => {
+    for (var style in styles) {
+        for (var k in styles[style]) {
+            if (ELASTIC_MAP[k] && ELASTIC_MAP[k][PR]) {
+                styles[style][k] = styles[style][k] * ELASTIC_MAP[k][PR];
             }
         }
-
-        return styles;
     }
+
+    return styles;
+};
+
+let create = (styles) => {
+    return StyleSheet.create(repair(styles));
+};
+
+export default {
+    repair: repair,
+    create: create
 };
